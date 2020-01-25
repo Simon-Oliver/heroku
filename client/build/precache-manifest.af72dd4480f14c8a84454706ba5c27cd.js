75b6e71f5473f21fec92738fe8513b707bfa5007
Simon-Oliver <stauffer.simon@outlook.com>
2020-01-25 19:12:52 +0100

    updated gitignore

---
+++ client/build/precache-manifest.af72dd4480f14c8a84454706ba5c27cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d51b82ce02e2bf697351eda88f313b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e7bd7b1aab4d956d325",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c602889a52c79ca934b",
+    "url": "/static/js/2.b2ced33c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b2ced33c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e7bd7b1aab4d956d325",
+    "url": "/static/js/main.58727f67.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
