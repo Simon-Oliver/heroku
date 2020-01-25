049fd470dcdcf1dd8d969a7826cc09d4d00e571d
Simon-Oliver <stauffer.simon@outlook.com>
2020-01-25 23:49:40 +0100

    added mapbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+import mapboxgl from 'mapbox-gl';
 import './App.css';
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2ltb24tb2xpdmVyIiwiYSI6ImNrNXUyZ2U4bDBxeXQzbmxvY3piaXA3eXcifQ.XL9r5bMRqb-yAyHi0dRj3Q';
 
 class App extends React.Component {
   state = {
     lat: '',
     long: '',
-    message: ''
+    message: '',
+    acc: '',
+    zoom: '2'
   };
 
   componentDidMount() {
-    if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(this.getPosition);
-    }
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    const success = pos => {
+      var crd = pos.coords;
+
+      this.setState({ lat: crd.latitude, long: crd.longitude, acc: crd.accuracy });
+      console.log(`More or less ${crd.accuracy} meters.`);
+    };
+
+    const error = err => {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lat, this.state.long],
+      zoom: this.state.zoom
+    });
+
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        trackUserLocation: true
+      })
+    );
 
     fetch('/api/hello')
       .then(res => res.json())
       .then(data => this.setState({ message: data.express }));
-
-    var layer = window.ga.layer.create('ch.swisstopo.pixelkarte-farbe');
-    var map = new window.ga.Map({
-      target: 'map',
-      layers: [layer],
-      view: new window.ol.View({
-        resolution: 500,
-        center: [this.lat, this.long]
-      })
-    });
   }
-
-  getPosition = position => {
-    this.setState({ lat: position.coords.latitude, long: position.coords.longitude });
-  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <h2>Lat: {this.state.lat}</h2>
           <h2>Long: {this.state.long}</h2>
+          <h2>Accuracy: {this.state.acc} meters</h2>
           <h3>{this.state.message}</h3>
-          <div id="map" class="map"></div>
+          <div className="container">
+            <div ref={el => (this.mapContainer = el)} className="mapContainer" />
+          </div>
         </header>
       </div>
     );
