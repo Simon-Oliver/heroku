967139b8dd62d32bdd5cb24847389747cf3ed4ce
Simon-Oliver <stauffer.simon@outlook.com>
2020-01-25 11:07:51 +0100

    fixing server for heroku

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
-const app = express();
-const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+const app = express();
+
+// Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req, res) => {
+  var list = ['item1', 'item2', 'item3'];
+  res.json(list);
+  console.log('Sent list of items');
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
